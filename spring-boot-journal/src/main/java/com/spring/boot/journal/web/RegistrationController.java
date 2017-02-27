bcdb793371bcda1ec02138e6b93b2b49128d5695
francescoesposito7 <francescoesposito7@gmail.com>
2017-02-27 13:03:55 +0100

    francescoesposito7

--- spring-boot-journal/src/main/java/com/spring/boot/journal/web/RegistrationController.java
+++ spring-boot-journal/src/main/java/com/spring/boot/journal/web/RegistrationController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+import com.spring.boot.journal.entities.Utilisateur;
+import com.spring.boot.journal.repository.UtilisateurRepository;
 import com.spring.boot.journal.service.UserService;
 
 @Controller
@@ -21,19 +24,25 @@ public class RegistrationController {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UtilisateurRepository userRepository;
 
 	
 	 @RequestMapping(value = "/registrationConfirm", method = RequestMethod.GET)
 	    public String confirmRegistration(final Locale locale, final Model model, @RequestParam("token") final String token) throws UnsupportedEncodingException {
 	        
 	    	final String result = userService.validateVerificationToken(token);
-	        
+	    	final Utilisateur user = userService.getUser(token);
+	    	
 	    	if (result.equals("valid")) {
 
 	            model.addAttribute("message", messages.getMessage("message.accountVerified", null, locale));
 	            return "redirect:/login";
 	        }
 
+	    	userRepository.delete(user);
+	    	
 	        model.addAttribute("message", messages.getMessage("auth.message." + result, null, locale));
 	        model.addAttribute("expired", "expired".equals(result));
 	        model.addAttribute("token", token);
