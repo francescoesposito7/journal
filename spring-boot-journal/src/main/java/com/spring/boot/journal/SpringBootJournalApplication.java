46b676d0ae4519675abca69d7ea6179d4543fbfc
Hugo Hyolle <hugo@hyolle.org>
2017-03-31 16:59:43 +0200

    Signed-off-by: Hugo Hyolle <hugo@hyolle.org>

--- spring-boot-journal/src/main/java/com/spring/boot/journal/SpringBootJournalApplication.java
+++ spring-boot-journal/src/main/java/com/spring/boot/journal/SpringBootJournalApplication.java
@@ -1,41 +1,21 @@
 package com.spring.boot.journal;
 
-import java.io.File;
-import java.io.FileInputStream;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.spring.boot.journal.entities.ImageUser;
-import com.spring.boot.journal.repository.ImageUserRepository;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootJournalApplication implements CommandLineRunner {
+public class SpringBootJournalApplication extends SpringBootServletInitializer {
 	
-	@Autowired
-	private ImageUserRepository imageRepository;
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootJournalApplication.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJournalApplication.class, args);
 	}
-
-	@Override
-	public void run(String... arg0) throws Exception {
-		
-		/*ImageUser imageUser = new ImageUser();
-		
-		File file = new File("C:\\Users\\94010-24-02\\git\\journal\\spring-boot-journal\\src\\main\\resources\\static\\img\\avatar_defaut.png");
-		
-		byte[] data = IOUtils.toByteArray(new FileInputStream(file));
-		
-		imageUser.setData(data);
-		
-		imageUser.setName("avatar_defaut.png");
-		
-		imageRepository.save(imageUser);*/
-		
-	}
+	
+	
 }
