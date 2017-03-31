c3280c33b2990bb062eed40bad3484714ee4826c
Hugo Hyolle <hugo@hyolle.org>
2017-03-31 15:58:48 +0200

    mot de passe oublie

--- spring-boot-journal/src/main/java/com/spring/boot/journal/SpringBootJournalApplication.java
+++ spring-boot-journal/src/main/java/com/spring/boot/journal/SpringBootJournalApplication.java
@@ -2,20 +2,13 @@ package com.spring.boot.journal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootJournalApplication extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootJournalApplication.class);
-    }
+
+public class SpringBootJournalApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJournalApplication.class, args);
 	}
-	
-	
+
 }
--- spring-boot-journal/src/main/java/com/spring/boot/journal/web/EntryController.java
+++ spring-boot-journal/src/main/java/com/spring/boot/journal/web/EntryController.java
@@ -1,19 +1,17 @@
 package com.spring.boot.journal.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.boot.journal.entities.Contact;
+import com.spring.boot.journal.entities.MotDePasseOublie;
 import com.spring.boot.journal.views.Views;
-
 
 @Controller
 public class EntryController{
-	
-
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView modelAndView){
@@ -43,6 +41,13 @@ public class EntryController{
 	@RequestMapping(value="/contactSuccess", method = RequestMethod.GET)
 	public ModelAndView contactSuccess(ModelAndView modelAndView){
 		modelAndView.setViewName(Views.VIEW_CONTACT_SUCCESS.getPage());
+		return modelAndView;
+	}
+	
+	@GetMapping(value="/motDePasseOublie")
+	public ModelAndView motDePasseOublie(ModelAndView modelAndView){
+		modelAndView.addObject("mdpOublie", new MotDePasseOublie());
+		modelAndView.setViewName(Views.VIEW_MOT_DE_PASSE_OUBLIE.getPage());
 		return modelAndView;
 	}
 	
